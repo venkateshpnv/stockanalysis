f34224e5d9531f2663a1d6ef74daaffaf02737e5
Venkatesh <venkateshpnv@gmail.com>
2019-02-25 18:32:47 -0800

    Selenium sample code

---
+++ sel.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox()
+#driver.get("http://www.google.com")
+#elem = driver.find_element_by_name("q")
+driver.get("http://www.ratestar.in/home")
+elem = driver.find_element_by_name("txtStock")
+#driver.get("http://www.python.org")
+#assert "Python" in driver.title
+#elem = driver.find_element_by_name("q")
+
+stock='LT Foods Ltd.'
+elem.clear()
+elem.send_keys(stock, Keys.ARROW_DOWN)
+time.sleep(2)
+elem.send_keys(Keys.RETURN)
+
+time.sleep(20)
+#try:
+#    element = WebDriverWait(driver, 100).until(EC.title_contains((By.ID, stock)))
+#finally:
+#    driver.close()
+
+assert "No results found." not in driver.page_source
+time.sleep(100)
+driver.close()
+
+
