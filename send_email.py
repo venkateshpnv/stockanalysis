49ab15e223976bfdad354283bd464fb1fb749247
Venkatesh Petla <petlanvenkatesh@gmail.com>
2019-06-19 15:36:34 -0700

    Added send email code

---
+++ send_email.py
@@ -0,0 +1,11 @@
+from calculations import calculate_dcf_all_stocks
+import DB
+import internet
+from common import *
+from excel import *
+
+def send_email():
+    get_radar_stocks()
+
+send_email()
+
