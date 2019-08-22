583c590e45ffcd16b5c1b55c4155617f09a31f9a
Venkatesh Petla <petlanvenkatesh@gmail.com>
2019-08-22 00:54:31 -0700

    Automatic Code Backup

--- scripts/autocheckin.sh
+++ scripts/autocheckin.sh
@@ -4,6 +4,7 @@ HOME_DIR=/home/vpetla/work/stockanalysis
 
 echo "Automatic DB Backup"
 mongodump --db=Stocks --out=$HOME_DIR/db_backup
+cd $HOME_DIR && git pull origin master
 cd $HOME_DIR && git add db_backup/*
 cd $HOME_DIR && git commit -m "Automatic DB Backup"
 
