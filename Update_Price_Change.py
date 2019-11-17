619bd48fcd34533c2b8378c7df31a1b60dbc018d
Venkatesh Petla <petlanvenkatesh@gmail.com>
2019-11-16 22:36:21 -0800

    Merged Update price change scripts for both US and India

---
+++ Update_Price_Change.py
@@ -0,0 +1,31 @@
+import sys
+import internet
+import DB
+from common import *
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Invalid arguments")
+        print("$ %s country_name" %(sys.argv[0]))
+        sys.exit(1)
+
+    ## Get today's price from yahoo and update db and hdf5
+    try:
+        DB.update_all_price_volume_db(sys.argv[1])
+    except Exception as e:
+        s = parse_html.html_head()
+        error = [str(e)]
+        s = parse_html.html_text(s, error)
+        internet.send_email2(sender_email_id, sender_passwd, receiver_email_id, "%s Update Price Volume Error" %(sys.argv[1]), s)
+
+    ## Calculate and store price change
+    try:
+        internet.update_all_stocks_price_change(sys.argv[1])
+    except Exception as e:
+        s = parse_html.html_head()
+        error = [str(e)]
+        s = parse_html.html_text(s, error)
+        internet.send_email2(sender_email_id, sender_passwd, receiver_email_id, "%s Update Price Change Error" %(sys.argv[1]), s)
+    
+    ## send email
+    internet.send_email_price_changes(sys.argv[1])
