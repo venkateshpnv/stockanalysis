5d779a64662cc357e768f9e4b74fed008f847fcf
Venkatesh Petla <petlanvenkatesh@gmail.com>
2020-03-13 23:07:09 -0700

    Updated US_new_listings.py to populate all stocks that are not parsed

--- US_new_listings.py
+++ US_new_listings.py
@@ -3,8 +3,8 @@ import DB
 if __name__ == "__main__":
     new_stocks = DB.build_US_All_Stocks_List()
     print("Number of new stocks: %r" %(new_stocks))
-    if new_stocks > 0:
-        DB.build_US_all_stock_information()
-        DB.set_sno('US')
+    DB.build_US_all_stock_information()
+    DB.set_sno('US')
+    if new_stocks > 1:
         DB.build_US_all_EPS()
         DB.update_US_all_stk_profile()
