3fffa4b58a7c008358698a5a5055ca2a8046f429
Venkatesh Petla <petlanvenkatesh@gmail.com>
2020-04-24 19:41:29 -0700

    import fixes

--- Update_Percent_Change.py
+++ Update_Percent_Change.py
@@ -1,5 +1,5 @@
-import hdf5
 import sys
+import internet
 import parse_html
 
 if __name__ == "__main__":
@@ -8,10 +8,12 @@ if __name__ == "__main__":
         print("$ %s country_name" %(sys.argv[0]))
         sys.exit(1)
 
+    #### Calculate and store price change
     try:
-        hdf5.update_percent_change_all(sys.argv[1])
+        internet.update_all_stocks_price_change(sys.argv[1])
     except Exception as e:
         s = parse_html.html_head()
         error = [str(e)]
         s = parse_html.html_text(s, error)
-        internet.send_email2(sender_email_id, sender_passwd, receiver_email_id, "%s Update Percent Change Error" %(sys.argv[1]), s)
+        internet.send_email2(sender_email_id, sender_passwd, receiver_email_id, "%s Update Price Change Error" %(sys.argv[1]), s)
+ 
