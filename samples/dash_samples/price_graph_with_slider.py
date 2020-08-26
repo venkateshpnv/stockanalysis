2f24238f57d220a66c3b4941204b73e34417627b
Venkatesh Petla <petlanvenkatesh@gmail.com>
2020-08-25 22:42:31 -0700

    Implemented stock buy sell

--- samples/dash_samples/price_graph_with_slider.py
+++ samples/dash_samples/price_graph_with_slider.py
@@ -13,6 +13,10 @@ import plotly.graph_objs as go
 
 from datetime import date, timedelta, datetime as dt
 import pandas as pd
+import pprint
+import pandas_ta as ta
+
+pp = pprint.PrettyPrinter(indent=4)
 
 def zoom(layout, xrange):
     in_view = df.loc[fig.layout.xaxis.range[0]:fig.layout.xaxis.range[1]]
@@ -22,58 +26,49 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-mysql_engine = DB.open_sql_connection('localhost', 'root', 'petla123', db='US_Stocks')
-query = 'select Date, `Adj Close`, `Volume` from {} order by Date'.format('STKAAPL')
-df = DB.read_from_sql(query, mysql_engine)
-DB.close_sql_connection(mysql_engine)
-graph_df = df
+def get_stock_data(symbol):
+	mysql_engine = DB.open_sql_connection('localhost', 'root', 'petla123', db='US_Stocks')
+	query = 'select `Date`, `Open`, `Close`, `High`, `Low`, `Adj Close` from {} order by Date'.format('STK'+symbol)
+	df = DB.read_from_sql(query, mysql_engine)
+	DB.close_sql_connection(mysql_engine)
+	return df
 
-years = len(pd.to_datetime(df.index).year.unique())
-mark_divisions = years / 2
-if mark_divisions < 2:
-    mark_divisions = 4
-mark_division_factor = int(len(df.index)/mark_divisions)
+df = get_stock_data('AAPL')
+df['RSI'] = ta.rsi(df['Adj Close'])
 
-date_list = {i: str(d) for i, d in enumerate(pd.to_datetime(df.index).year.unique())}
-date_list = {}
-for i, d in enumerate(pd.to_datetime(df.index)):
-    if i % int(len(df.index)/mark_divisions) == 0:
-        date_list[i] = str(dt.strptime(str(d.year), "%Y").date())
-date_list[i] = 'till_date'
-
-print(date_list)
-min_year = pd.to_datetime(graph_df.index[0]).year
-max_year = pd.to_datetime(graph_df.index[-1]).year
+#years = len(pd.to_datetime(df.index).year.unique())
+#mark_divisions = years / 2
+#if mark_divisions < 2:
+#    mark_divisions = 4
+#mark_division_factor = int(len(df.index)/mark_divisions)
+#
+#date_list = {i: str(d) for i, d in enumerate(pd.to_datetime(df.index).year.unique())}
+#date_list = {}
+#for i, d in enumerate(pd.to_datetime(df.index)):
+#    if i % int(len(df.index)/mark_divisions) == 0:
+#        date_list[i] = str(dt.strptime(str(d.year), "%Y").date())
+#date_list[i] = 'till_date'
+#
+#print(date_list)
+#min_year = pd.to_datetime(graph_df.index[0]).year
+#max_year = pd.to_datetime(graph_df.index[-1]).year
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Stock Prices'),
 
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
 
+    #dcc.Input(id='input-box', value='', type='text', placeholder='Enter a stock symbol', ),
+    #html.Button('Submit', id='button'),
+
     dcc.Graph(
-        id='candle-graph', animate=True, style={'backgroundColor':'#1a2d46', 'color':'#ffffff'},),
+        id='stock-graph',
+        animate=True
+        #style={'backgroundColor':'#1a2d46', 'color':'#ffffff'},
+        )
 
-    html.Br(),
-    html.Br(),
-    html.Br(),
-
-    html.P(id="p"),
-
-    dcc.RangeSlider(
-        id='range-slider',
-        #min = min_year, 
-        #max = max_year, 
-        min = 0,
-        max = len(graph_df.index),
-        step = None,
-        marks=date_list,
-        #allowCross=True,
-        value = [0, len(graph_df.index)],
-        vertical=True,
-        verticalHeight=400
-    )
 ])
 
 def get_start_end(df, points):
@@ -96,32 +91,69 @@ def get_start_end(df, points):
     return start, end 
 
 @app.callback(
-    #Output('p', 'title'),
-    Output('candle-graph', 'figure'),
-    [Input('candle-graph', 'relayoutData')])
-def hover(points):
+    Output('stock-graph', 'figure'),
+    [Input('stock-graph', 'relayoutData')] 
+    #Input('button', 'n_clicks')
+    #],
+    #[State('input-box', 'value')]
+   )
+def price_graph(points): # n_clicks, symbol):
     print("Hover Value : {}".format(points))
     if points:
         print("Hover keys : {}".format(points.keys()))
 
-    #if points == None or 'autosize' in points.keys() or 'xaxis.autorange' in points.keys():
-    #    graph_df = df 
-    #else:
-    if True:
-        graph_df = df
-        start, end = get_start_end(df, points)
-        if start and end:
-            graph_df = df.loc[start:end]
+    #print("Symbol: %s" %(symbol))
+    #if not symbol:
+    #    symbol = 'AAPL'
+
+    #df = get_stock_data(symbol.upper())
+
+    #df['RSI'] = ta.rsi(df['Adj Close'])
+
+    graph_df = df
+    start, end = get_start_end(df, points)
+    if start and end:
+        graph_df = df.loc[start:end]
+
+    sell_graph = go.Scatter(
+           x = graph_df['Adj Close'][graph_df['RSI']>70].index,
+           y = graph_df['Adj Close'][graph_df['RSI']>70],
+           mode = 'markers',
+           name = 'Sell',
+           fillcolor = 'rgb(128,0,0)'
+           )
+
+    buy_graph = go.Scatter(
+           x = graph_df['Adj Close'][graph_df['RSI']<35].index,
+           y = graph_df['Adj Close'][graph_df['RSI']<35],
+           mode = 'markers',
+           name = 'Buy',
+           fillcolor = 'rgb(0,128,0)'
+           )
 
     line_graph = go.Scatter(
             x = graph_df.index,
-            y = graph_df['Adj Close']
+            y = graph_df['Adj Close'],
+            name = 'Price'
             #x = graph_df.index[value[0]:value[1]],
             #y = graph_df['Adj Close'][value[0]:value[1]]
             #x = df.index,
             #y = df['Adj Close']
             )
-  
+ 
+    candlestick_graph = go.Candlestick(
+            x = graph_df.index,
+            open = graph_df['Open'] + 50,
+            high = graph_df['High'] + 50,
+            low  = graph_df['Low']  + 50,
+            close= graph_df['Close']+ 50,
+            increasing = {'line': {'color' : '#00CC94'}},
+            decreasing = {'line': {'color' : '#F50030'}},
+            name = 'Candlestick'
+            ) 
+    data = [line_graph, sell_graph, buy_graph]
+    #data = [sell_graph, buy_graph, line_graph, candlestick_graph]
+            
     #layout = go.Layout()
     layout = go.Layout(
             #paper_bgcolor='#27293d',
@@ -135,27 +167,30 @@ def hover(points):
                               dict(count=6, label="6m", step="month", stepmode="backward"),
                               dict(count=1, label="YTD", step="year", stepmode="todate"),
                               dict(count=1, label="1y", step="year", stepmode="backward"),
-                              dict(count=50, label="50y", step="year", stepmode="backward"),
+                              dict(count=5, label="5y", step="year", stepmode="backward"),
+                              dict(count=10, label="10y", step="year", stepmode="backward"),
                               dict(step="all")
                           ])
                        ),
-                       rangeslider=dict(visible=True),
+                       #rangeslider=dict(visible=True,
+                       #                 range=[df.index.min(), df.index.max()]), 
                        range=[graph_df.index.min(), graph_df.index.max()], 
                        #autorange=True, automargin=True,
                        fixedrange=False,
                        type="date"
                        ),
             yaxis=dict(range=[graph_df['Adj Close'].min(), 
-                       graph_df['Adj Close'].max()], 
+                       graph_df['Adj Close'].max()+100], 
                        #autorange=True, 
                        #automargin=True,
                        fixedrange=False
                       ),
                       #font=dict(color='white'),
             )
-
-    data = [line_graph]
-
+   # pp.pprint(layout)
+     
+    #fig = go.Figure(data, layout)
+    #return fig 
     return {'data':data, 'layout':layout}
 
 if __name__ == '__main__':
