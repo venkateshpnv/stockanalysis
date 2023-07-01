1356930399c23e75d30177164b2c0c415a53c04e
Venkatesh Petla <petlanvenkatesh@gmail.com>
2023-07-01 16:50:50 -0500

    Send RSI Email

--- Update_Price_Change.py
+++ Update_Price_Change.py
@@ -57,6 +57,7 @@ if __name__ == "__main__":
     try:
         if len(sys.argv) == 2:
             internet.send_email_price_changes('US')
+            internet.send_email_rsi_changes('US')
     except Exception as e:
         print('Failed to send email, err: %s' %(str(e)))
         pass
